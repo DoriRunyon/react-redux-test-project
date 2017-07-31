60d84d110d6a2047f468e386de96e102591dcbf9
DoriRunyon <dorileerunyon@gmail.com>
2017-07-31 16:40:03 -0700

    Created first component. Set up React Router in index.js.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
+import PokemonSearch from './components/pokemon_search';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <BrowserRouter>
+   <div>
+    <Switch>
+        <Route path="/" component={PokemonSearch} />
+    </Switch>
+   </div>
+  </BrowserRouter>
+
   </Provider>
   , document.querySelector('.container'));
